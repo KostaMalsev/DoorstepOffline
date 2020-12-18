9a8a86bed26e62ddddbec4c2e8e29a8dee6e1db8
KostaMalsev <kostya.malsev@gmail.com>
2020-12-18 14:29:40 +0200

    1

--- VideoChatHelper.js
+++ VideoChatHelper.js
@@ -29,7 +29,7 @@ var yourVideo = document.getElementById("yourVideo");
 var friendsVideo = document.getElementById("friendsVideo");
 var yourId = Math.floor(Math.random()*1000000000);
 //Create an account on Viagenie (http://numb.viagenie.ca/), and replace {'urls': 'turn:numb.viagenie.ca','credential': 'websitebeaver','username': 'websitebeaver@email.com'} with the information from your account
-var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'} /*, {'urls': 'stun:stun.l.google.com:19302'}*/, {'urls': 'numb.viagenie.ca:3478','credential': 'limor100','username': 'kostya.malsev@gmail.com'}]};
+var servers = {'iceServers': [{'urls': 'stun:stun.services.mozilla.com'} /*, {'urls': 'stun:stun.l.google.com:19302'}*/, {'urls': 'numb.viagenie.ca','credential': 'limor100','username': 'kostya.malsev@gmail.com'}]};
 var pc = new RTCPeerConnection(servers);
 pc.onicecandidate = (event => event.candidate?sendMessage(yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") );
 pc.onaddstream = (event => friendsVideo.srcObject = event.stream);
