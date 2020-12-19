a512618e83828b78db01964347a6bf9d2cb6074e
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2020-12-19 19:16:05 +0200

    Update main.js
    update log message function

--- public/main.js
+++ public/main.js
@@ -6,9 +6,14 @@ let myVideoEl = document.querySelector('.my-video');
 let button = document.querySelector('.button');
 let loaderSVG = '<svg class="loader2" width="32" height="32" viewBox="0 0 100 100"><rect fill="white" height="6" opacity="0" rx="3" ry="3" transform="rotate(-90 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.08333333333333333" rx="3" ry="3" transform="rotate(-60 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.16666666666666666" rx="3" ry="3" transform="rotate(-30 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.25" rx="3" ry="3" transform="rotate(0 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.3333333333333333" rx="3" ry="3" transform="rotate(30 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.4166666666666667" rx="3" ry="3" transform="rotate(60 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.5" rx="3" ry="3" transform="rotate(90 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.5833333333333334" rx="3" ry="3" transform="rotate(120 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.6666666666666666" rx="3" ry="3" transform="rotate(150 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.75" rx="3" ry="3" transform="rotate(180 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.8333333333333334" rx="3" ry="3" transform="rotate(210 50 50)" width="25" x="72" y="47"></rect><rect fill="white" height="6" opacity="0.9166666666666666" rx="3" ry="3" transform="rotate(240 50 50)" width="25" x="72" y="47"></rect></svg>';
 
+
+
+
 let logMessage = (message) => {
   let newMessage = document.createElement('div');
   newMessage.innerHTML = message;
+  messagesEl.innerHTML="";
+  //if(messagesEl.childElementCount>0){
   messagesEl.appendChild(newMessage);
 };
 
@@ -78,7 +83,7 @@ peer.on('connection', (conn) => {
   });
   conn.on('data', (data) => {
     //messagesEl.children[messagesEl.children.length - 1].remove();
-    //logMessage(JSON.parse(data));
+    logMessage(JSON.parse(data));
     rotateCamera(data);
   });
 });
@@ -149,6 +154,7 @@ let sendGyroData = (data) => {
   if (peerConn) {
     // Rotate scene camera
     rotateCamera(data);
+    //logMessage(`alpha = ${data.alpha.toFixed(1)} beta=${data.beta.toFixed(1)`);
     // Send data
     peerConn.send(data);
   }
