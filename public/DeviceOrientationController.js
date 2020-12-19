d9a2f524ce0aacdc73447bad0a2d3f590f9fa3b9
KostaMalsev <kostya.malsev@gmail.com>
2020-12-19 21:07:05 +0200

    1

--- public/DeviceOrientationController.js
+++ public/DeviceOrientationController.js
@@ -408,7 +408,7 @@ var DeviceOrientationController = function ( object, domElement ) {
       this.deviceOrientation.gamma = e.detail.gamma;
       //Update rotation with new data:
       this.updateDeviceMove();
-    }();//.bind( this );
+    }.bind( this );
 
 
     this.updateDeviceMove = function () {
