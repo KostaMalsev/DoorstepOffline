38f261aeff5718bfbf9ed9e25e340c26c912944b
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2020-12-19 19:31:02 +0200

    Update main.js

--- public/main.js
+++ public/main.js
@@ -158,7 +158,7 @@ let sendGyroData = (data) => {
     // Send data
     peerConn.send(data);
     
-    messagesEl.innerHTML = JSON.parse(data);
+    messagesEl.innerHTML = JSON.stringify(data);
   }
 }
 
