6cf787e9ebaf3efc4a7ee0d4d81207160190e9a9
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2020-12-20 12:54:15 +0200

    Update world.js

--- public/world.js
+++ public/world.js
@@ -10,7 +10,9 @@
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(0, 0, 0.1);
-var renderer = new THREE.WebGLRenderer( { alpha: true } );
+var renderer = new THREE.WebGLRenderer({
+  alpha: true
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 renderer.domElement.style.position = 'fixed';
@@ -34,7 +36,7 @@ document.body.appendChild(cssRenderer.domElement);
 // Create plane at z position of "-5" in front of the camera
 var normal = new THREE.Vector3(0, 0, -1);
 var center = new THREE.Vector3();
-var plane = new THREE.Plane().setFromNormalAndCoplanarPoint(normal,center);
+var plane = new THREE.Plane().setFromNormalAndCoplanarPoint(normal, center);
 
 // Align the geometry to the plane
 // Create a basic rectangle geometry
@@ -45,7 +47,10 @@ planeGeometry.lookAt(focalPoint);
 planeGeometry.translate(coplanarPoint.x, coplanarPoint.y, coplanarPoint.z);
 
 // Draw reference blue plane
-const material2 = new THREE.MeshBasicMaterial({color: 'blue', side: THREE.DoubleSide});//MeshLambertMaterial
+const material2 = new THREE.MeshBasicMaterial({
+  color: 'blue',
+  side: THREE.DoubleSide
+}); //MeshLambertMaterial
 const mesh1 = new THREE.Mesh(planeGeometry, material2);
 mesh1.rotation.x = 0;
 mesh1.position.y = 0;
@@ -54,11 +59,15 @@ mesh1.position.x = 0;
 scene.add(mesh1);
 
 // Create forward plane (to tap on)
-const material1 = new THREE.MeshBasicMaterial({color: 0x000000, alphaTest: 0, visible: false});
+const material1 = new THREE.MeshBasicMaterial({
+  color: 0x000000,
+  alphaTest: 0,
+  visible: false
+});
 const mesh = new THREE.Mesh(new THREE.PlaneGeometry(window.innerWidth, window.innerHeight), material1);
 mesh.rotation.x = 0;
 mesh.position.y = 0;
-mesh.position.z = -10;//-80
+mesh.position.z = -10; //-80
 mesh.position.x = 0;
 
 // Bind the plane with roating camera
@@ -72,11 +81,11 @@ cssRenderer.domElement.addEventListener('click', clickedOnScreen);
 
 //Create device binded controls:
 //They will listen also to rotation from net
-var DevControls = new DeviceOrientationController( camera, cssRenderer.domElement );
+var DevControls = new DeviceOrientationController(camera, cssRenderer.domElement);
 DevControls.connect();
 
 // Set resize (reshape) callback
-window.addEventListener( 'resize', resize );
+window.addEventListener('resize', resize);
 
 // Create raycaster
 var raycaster = new THREE.Raycaster();
@@ -84,14 +93,14 @@ var mouse = new THREE.Vector2();
 var tempRadius = new THREE.Vector3();
 
 // Sample mouse readings
-function setMouse(event){
-  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+function setMouse(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
 //Set raycaster from camera:
-function setRaycaster(event){
-	setMouse(event);
+function setRaycaster(event) {
+  setMouse(event);
   raycaster.setFromCamera(mouse, camera);
 }
 
@@ -104,7 +113,11 @@ function clickedOnScreen(event) {
 
   // If we got intersection, make a marker on xyz point
   if (intersects.length > 0) {
-    let pt = {x:intersects[0].point.x, y: intersects[0].point.y, z: intersects[0].point.z};
+    let pt = {
+      x: intersects[0].point.x,
+      y: intersects[0].point.y,
+      z: intersects[0].point.z
+    };
     createPoint(pt);
     sendMarker(pt);
   }
@@ -120,31 +133,30 @@ function createPoint(pt) {
   var vricon = new CSS2DObject(element1);
 
   scene2.add(vricon);
-  vricon.position.x = pt.x;//intersects[0].point.x;
-  vricon.position.y = pt.y;//intersects[0].point.y;
-  vricon.position.z = pt.z;//intersects[0].point.z + 0.1;
+  vricon.position.x = pt.x; //intersects[0].point.x;
+  vricon.position.y = pt.y; //intersects[0].point.y;
+  vricon.position.z = pt.z; //intersects[0].point.z + 0.1;
 }
 
 function resize() {
-	width = window.innerWidth;
-	height = window.innerHeight;
-	camera.right = width;
-	camera.bottom = height;
-	camera.updateProjectionMatrix();
-	renderer.setSize(width,height);
-  cssRenderer.setSize(width,height);
-	render();
+  width = window.innerWidth;
+  height = window.innerHeight;
+  camera.right = width;
+  camera.bottom = height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(width, height);
+  cssRenderer.setSize(width, height);
+  render();
 }
 
-function render(){
-	requestAnimationFrame(render);
+function render() {
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
   cssRenderer.render(scene2, camera);
   DevControls.update();
 }
 
 render();
-
 
 // Rotate camera with pitch, roll, yual
 function rotateCamera(data) {
