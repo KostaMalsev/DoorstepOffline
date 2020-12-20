5f88af215a5ed6587e4fc22335782fb25f2d344e
KostaMalsev <kostya.malsev@gmail.com>
2020-12-20 21:28:14 +0200

    1

--- public/world.js
+++ public/world.js
@@ -97,7 +97,7 @@ function resizeSphere(width, height) {
   const mesh = new THREE.Mesh(new THREE.PlaneGeometry(width, height), material1);
   mesh.rotation.x = 0;
   mesh.position.y = 0;
-  mesh.position.z = -800; //-10 -80
+  mesh.position.z = -200; //-10 -80
   mesh.position.x = 0;
   camera.add(mesh)
 }
