a7f6009f8f6aed5cc3bad9186eb98bf1424664fa
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2021-02-19 14:09:44 +0200

    a

--- min/minify.cmd
+++ min/minify.cmd
@@ -2,13 +2,9 @@
 title Prep for Minifier
 cls
 
-echo Appending libraries
-cd C:\Users\limor\Downloads\doorstepnetlify\src\lib\
-copy /b peerjs.min.js+three.min.js+CSS2DRenderer.js C:\Users\limor\Downloads\doorstepnetlify\min\lib.js
-
 echo Appending scripts
 cd C:\Users\limor\Downloads\doorstepnetlify\src\slave\
-copy /b DeviceOrientationController.js+world.js+gyro.js C:\Users\limor\Downloads\doorstepnetlify\min\doorstep.js
+copy /b C:\Users\limor\Downloads\doorstepnetlify\src\lib\CSS2DRenderer.js+DeviceOrientationController.js+world.js+gyro.js C:\Users\limor\Downloads\doorstepnetlify\min\doorstep.js
 
 echo.
 echo "Done. Minify @ https://jscompress.com/ & Obfusticate @ https://www.obfuscator.io/"
